b47c67b8707845c1e9027edf7b9af2ba38f94795
Rohan Raval <rohanraval@gmail.com>
2017-08-09 22:52:50 -0400

    solved successor

--- CTCI4_TreesGraphs/successor.java
+++ CTCI4_TreesGraphs/successor.java
@@ -2,3 +2,31 @@
 Successor: Write an algorithm to find the "next" node (i.e., in-order successor) of a given node in a
 binary search tree. You may assume that each node has a link to its parent. 
 */
+
+/* SOLUTION 1
+    successor(n) is the leftmost node of n's right subtree.
+    If no right subtree, then...
+        If n is the left child of n->parent, then the successor is n->parent
+        If n is the right child of n->parent, then go up until n is left child of n->parent
+*/
+Node successor (Node n) {
+    if (n == null) return null;
+
+    //if n has a right subtree get its leftmost child
+    if (n.right != null) {
+        n = n.right; //use the right subtree
+
+        //loop until you get to the leftmost child
+        while (n.left != null) {
+            n = n.left;
+        }
+        return n;
+    } 
+    //if no right subtree, loop upwards until n is a left child
+    else {
+        while (n.parent != null && n != n.parent.left) {
+            n = n.parent;
+        }
+        return n.parent; //if n.parent is null, that means we've reached the end
+    }
+}
